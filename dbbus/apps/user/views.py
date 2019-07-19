74b24ec02c50622c3e7c09047ac46dc373a5329b
Mo Fiebiger <moriah.fiebiger@ucdconnect.ie>
2019-07-19 12:58:46 +0100

    modularizing code and activated service workers

--- dbbus/apps/user/views.py
+++ dbbus/apps/user/views.py
@@ -16,26 +16,37 @@ from user.form import ForgetPwdForm,ResetPwdForm,ChangePwdForm
 from user.models import User, UserBusNumber, UserStop, UserRoute
 import json
 from django.http import QueryDict
+import config
 
-REGISTER_ENCRYPT_KEY = 'djhadhakjhfaliuehjdlaufajdhfalkfdkjiidd354/2p812p39weqklrjq/'
-FORGET_PASSWORD_ENCRYPT_KEY = 'SDLFJAIAOINCAJDHFAIUifdack123/.df/2p812p39weqklrjq/'
+# REGISTER_ENCRYPT_KEY = 'djhadhakjhfaliuehjdlaufajdhfalkfdkjiidd354/2p812p39weqklrjq/'
+# FORGET_PASSWORD_ENCRYPT_KEY = 'SDLFJAIAOINCAJDHFAIUifdack123/.df/2p812p39weqklrjq/'
+REGISTER_ENCRYPT_KEY = config.register_encrypt_key
+FORGET_PASSWORD_ENCRYPT_KEY = config.forget_password_encrypt_key
 
-  
-class IndexView(TemplateView):  
+
+class IndexView(TemplateView):
     '''首页'''
     def get(self, request):
         '''index page'''
         return render(request, 'index.html')
-            
-        
-        
+
+class TourismView(TemplateView):
+    def get(self, request):
+        return render(request, 'tourismPage.html')
+
+class serviceWorker(TemplateView):
+    template_name = 'js/serviceworker.js'
+    content_type = 'application/javascript'
+
+
+
 # /user/register
 class RegisterView(TemplateView):
     '''注册'''
     def get(self, request):
         '''显示注册页面'''
         return render(request, 'register.html')
-  
+
     def post(self, request):
         '''进行注册处理'''
         # 接收数据
@@ -107,10 +118,10 @@ class ActiveView(TemplateView):
         try:
             # 获取待激活用户的id
             user_id = serializer.loads(token)
-            user = User.objects.get(id=user_id) 
+            user = User.objects.get(id=user_id)
             if user.is_active == 1:
                 return HttpResponse('your email has been verified')
-            
+
             # 跳转到登录页面
             return render(request,'active.html',{'user':user})
         except SignatureExpired as e:
@@ -119,7 +130,7 @@ class ActiveView(TemplateView):
         except BadSignature as e:
             # 激活链接已过期
             return render(request,'404.html')
-        
+
     def post(self, request, token):
         '''get the user information to activate it'''
             # 根据id获取用户信息
@@ -128,12 +139,12 @@ class ActiveView(TemplateView):
             user_id = serializer.loads(token)
             user = User.objects.get(id=user_id)
             user.is_active = 1
-            user.save()        
+            user.save()
             return redirect(reverse('user:login'))
         except BadSignature as e:
             # 激活链接已过期
             return render(request,'404.html')
-  
+
 # /user/login
 class LoginView(TemplateView):
     '''登录'''
@@ -146,7 +157,7 @@ class LoginView(TemplateView):
         else:
             username = ''
             checked = ''
-  
+
         # 使用模板
         return render(request, 'login.html', {'username':username, 'checked':checked})
         # return render(request, 'index.html', {'username':username, 'checked':checked})
@@ -158,7 +169,7 @@ class LoginView(TemplateView):
         password = request.POST.get('pwd')
         # 校验数据
         if not all([username, password]):
-            return render(request, 'index.html', {'errmsg':'Data is not complete'}) 
+            return render(request, 'index.html', {'errmsg':'Data is not complete'})
         # 业务处理:登录校验
         user = authenticate(username=username, password=password)
         if user is not None:
@@ -175,13 +186,13 @@ class LoginView(TemplateView):
                 response = redirect(next_url) # HttpResponseRedirect
                 # 判断是否需要记住用户名
                 remember = request.POST.get('remember')
-                
+
                 if remember == 'on':
                     # 记住用户名
                     response.set_cookie('username', username, max_age=7*24*3600)
                 else:
                     response.delete_cookie('username')
-  
+
                 # 返回response
                 return response
             else:
@@ -191,8 +202,8 @@ class LoginView(TemplateView):
             # 用户名或密码错误
             print('3')
             return render(request, 'index.html', {'errmsg':'username or password wrong'})
-  
-  
+
+
 # /user/logout
 class LogoutView(TemplateView):
     '''退出登录'''
@@ -200,7 +211,7 @@ class LogoutView(TemplateView):
         '''退出登录'''
         # 清除用户的session信息
         logout(request)
-  
+
         # 跳转到首页
         return redirect(reverse('user:index'))
 
@@ -209,40 +220,40 @@ class LogoutView(TemplateView):
 
 class PasswordChangeView(LoginRequiredMixin, TemplateView):
     '''change password'''
-    
+
     def get(self,request):
         change_password_form=ChangePwdForm()
         return render(request,'pwd_change.html',{'change_password_form':change_password_form})
-    
-    
+
+
     def post(self, request):
-    
+
         username = request.user.username
         original_password = request.POST.get('original_pwd')
         new_password = request.POST.get('new_pwd')
-        rnew_password = request.POST.get('cnew_pwd')  
-             
+        rnew_password = request.POST.get('cnew_pwd')
+
         if not all([original_password,new_password, rnew_password]):
-            return render(request, 'pwd_change.html', {'errmsg':'Data is not complete'}) 
-        
+            return render(request, 'pwd_change.html', {'errmsg':'Data is not complete'})
+
         if new_password != rnew_password:
             return render(request, 'pwd_change.html', {'errmsg': 'the passwords are not match'})
-        
+
         user = request.user
         if user.check_password(original_password):
             request.user.set_password(new_password)
             user.save()
         return render(request, 'index.html')
 
-            
+
 class PasswordForgetView(TemplateView):
     '''change password'''
-     
+
     def get(self, request):
         ''''''
-        forget_form=ForgetPwdForm()           
+        forget_form=ForgetPwdForm()
         return render(request, 'pwd_forget.html',{'forget_form':forget_form})
-         
+
     def post(self, request):
         ''''''
         forget_form = ForgetPwdForm(request.POST)
@@ -254,11 +265,11 @@ class PasswordForgetView(TemplateView):
                 print(email)
             except User.DoesNotExist:
                 user = None
-            
+
             if user:
                 serializer = Serializer(FORGET_PASSWORD_ENCRYPT_KEY, 3600)
                 token = serializer.dumps(user.id).decode() # bytes
-          
+
                 # 发邮件
                 # 组织邮件信息
                 subject = 'Password forgotten'
@@ -267,14 +278,14 @@ class PasswordForgetView(TemplateView):
                 receiver = [email]
                 host_name = request.get_host()
                 # html_message = '<h1'+username+', This email is used to reset your password!</h1>please click the link below to reset your password.<br/><a href="http://'+host_name+'/user/reset_password/'+token+'">http://'+host_name+'/user/reset_password/'+token+'</a>'
-             
+
                 # send_mail(subject, message, sender, receiver, html_message=html_message)
                 return render(request,'send_success.html')
 
             return render(request,'pwd_forget.html',{'errormg':'email has not been found'})
         else:
             return render(request,'pwd_forget.html',{'forget_form':forget_form})
-            
+
 
 
 class ResetPasswordView(TemplateView):
@@ -284,7 +295,7 @@ class ResetPasswordView(TemplateView):
         try:
             # 获取待激活用户的id
             user_id = serializer.loads(token)
-            user = User.objects.get(id=user_id)            
+            user = User.objects.get(id=user_id)
             # 跳转到登录页面
             return render(request,'pwd_reset.html',{'user':user})
         except SignatureExpired as e:
@@ -293,8 +304,8 @@ class ResetPasswordView(TemplateView):
         except BadSignature as e:
             # 激活链接已过期
             return render(request,'404.html')
-        
-        
+
+
     def post(self,request,token):
         reset_form=ResetPwdForm(request.POST)
 
@@ -304,30 +315,30 @@ class ResetPasswordView(TemplateView):
 
             if new_password != rnew_password:
                 return render(request,'pwd_reset.html',{'msg':'password do not match！'})
-            
-            serializer = Serializer(FORGET_PASSWORD_ENCRYPT_KEY, 3600) 
+
+            serializer = Serializer(FORGET_PASSWORD_ENCRYPT_KEY, 3600)
             try:
                 user_id = serializer.loads(token)
                 user = User.objects.get(id=user_id)
                 user.set_password(new_password)
-                user.save()       
+                user.save()
                 return redirect(reverse('user:index'))
-            
+
             except BadSignature as e:
                 # 激活为坏
                 return render(request,'404.html')
 
         else:
-            return render(request,'pwd_reset.html',{'msg':reset_form.errors})                   
+            return render(request,'pwd_reset.html',{'msg':reset_form.errors})
 
-            
+
 class AvatarUpdateView(LoginRequiredMixin, TemplateView):
-    
-    def post(self,request): 
+
+    def post(self,request):
         user = request.user
         avatar = request.FILES.get('avatar')
         if not all([avatar]):
-           return HttpResponse('No image has been uploaded!') 
+           return HttpResponse('No image has been uploaded!')
         try:
             avatar.name = user.username + '.' + avatar.name.split('.')[-1]
             user.user_profile = avatar
@@ -335,13 +346,13 @@ class AvatarUpdateView(LoginRequiredMixin, TemplateView):
 #             img_name = 'avatar/'+ avatar.name
 #             image_path = os.path.join(settings.MEDIA_ROOT, img_name)
 # #             print(a)
-# 
+#
 #             with open(image_path,'wb') as f:
 #                 f.write(avatar.read())
-            print('ok')     
+            print('ok')
         except:
             return HttpResponse('error!')
-                  
+
 #         return JsonResponse(data)
         return render(request,'index.html')
 
@@ -674,22 +685,22 @@ class ContactUsView(LoginRequiredMixin, TemplateView):
             'Your message has been sent to the manager,we are very thankful, and we will contact to you as soon as possible!')
 
 
-       
-    
+
+
 
 class FavoriteStopView(LoginRequiredMixin, TemplateView):
     '''store the favorite stops of user'''
     def get(self,request):
-        
+
         stops = UserStop.objects.filter(station_user= request.user)
         stops = list(stops)
         stop_list = [stop.stop for stop in stops]
         json_file = {"user_stop_list": stop_list}
         return JsonResponse(json_file)
-            
-    
+
+
     def post(self,request):
-        
+
         stop_id = request.POST.get('stop_id')
         if UserStop.objects.filter(stop=stop_id,station_user= request.user).exists():
             return HttpResponse('stop exists already')
@@ -697,69 +708,69 @@ class FavoriteStopView(LoginRequiredMixin, TemplateView):
         user_stop.save()
         return HttpResponse('stop is stored successfully')
 
-    
+
     def delete(self, request):
-        
+
         DELETE = QueryDict(request.body)
         stop_id = DELETE.get('stop_id')
 
         if not UserStop.objects.filter(stop=stop_id,station_user= request.user).exists():
             return HttpResponse('stop number does not exist')
-        
+
         UserStop.objects.get(stop=stop_id,station_user= request.user).delete()
         return HttpResponse('stop number has been deleted successfully')
 
-        
+
 
 class FavoriteBusNumberView(LoginRequiredMixin, TemplateView):
     '''store the favorite bus numbers of user'''
-    
+
     def get(self,request):
-        
+
         buses = UserBusNumber.objects.filter(bus_number_user= request.user)
         buses = list(buses)
         buses_list = [{'bus_number':bus.bus_number,'start_point':bus.start_point,'end_point':bus.end_point} for bus in buses]
         json_file = {"user_bus_list": buses_list}
         return JsonResponse(json_file)
-    
+
     def post(self,request):
-        
+
         bus_number = request.POST.get('bus_number')
         start_point = request.POST.get('start_point')
         end_point = request.POST.get('end_point')
         if UserBusNumber.objects.filter(bus_number=bus_number,start_point=start_point,end_point=end_point,bus_number_user= request.user).exists():
-            return HttpResponse('bus number exists already')        
+            return HttpResponse('bus number exists already')
         user_bus_number = UserBusNumber(bus_number=bus_number,start_point= start_point,end_point=end_point,bus_number_user=request.user)
         user_bus_number.save()
         return HttpResponse('Bus number is stored successfully')
 
     def delete(self, request):
-        
+
         DELETE = QueryDict(request.body)
         bus_number = DELETE.get('bus_number')
         start_point = DELETE.get('start_point')
         end_point = request.DELETE.get('end_point')
         if not UserBusNumber.objects.filter(bus_number=bus_number,start_point=start_point,end_point=end_point,bus_number_user= request.user).exists():
             return HttpResponse('bus number does not exist')
-        
+
         UserBusNumber.objects.get(bus_number=bus_number,start_point=start_point,end_point=end_point,bus_number_user= request.user).delete()
         return HttpResponse('bus number has been deleted successfully')
 
 
 class FavoriteRouteView(LoginRequiredMixin, TemplateView):
     '''store the favorite routes of user'''
-    
+
     def get(self,request):
-        
+
         routes = UserRoute.objects.filter(route_user= request.user)
         routes = list(routes)
         routes_list = [{'route_start':route.route_start,'route_end':route.route_end} for route in routes]
         json_file = {"user_routes_list": routes_list}
         return JsonResponse(json_file)
-    
-    
+
+
     def post(self,request):
-        
+
         route_start = request.POST.get('route_start')
         route_end = request.POST.get('route_end')
         if UserRoute.objects.filter(route_start=route_start,route_end=route_end,route_user= request.user).exists():
@@ -767,34 +778,34 @@ class FavoriteRouteView(LoginRequiredMixin, TemplateView):
         user_route = UserRoute(route_start=route_start,route_end= route_end,route_user=request.user)
         user_route.save()
         return HttpResponse('route is stored successfully')
-    
+
     def delete(self, request):
-        
+
         DELETE = QueryDict(request.body)
         route_start = DELETE.get('route_start')
         route_end = DELETE.get('route_end')
         if not UserRoute.objects.filter(route_start=route_start,route_end=route_end,route_user= request.user).exists():
             return HttpResponse('route does not exist')
-        
+
         UserRoute.objects.get(route_start=route_start,route_end=route_end,route_user= request.user).delete()
         return HttpResponse('route has been deleted successfully')
 
 class ContactUsView(LoginRequiredMixin, TemplateView):
-    
-    def post(self,request): 
+
+    def post(self,request):
         user = request.user
         contact = request.POST.get('contact')
         if not contact:
-           return HttpResponse('No information!') 
+           return HttpResponse('No information!')
         try:
             subject = 'Contact information-from '+user.username+"-email:"+ user.email
             message = contact
             sender = settings.EMAIL_FROM
-            receiver = [settings.EMAIL_FROM]         
+            receiver = [settings.EMAIL_FROM]
             send_mail(subject, message, sender, receiver)
         except:
             return HttpResponse('error!')
-                  
+
 #         return JsonResponse(data)
         return HttpResponse('Your message has been sent to the manager,we are very thankful, and we will contact to you as soon as possible!')
 
@@ -802,228 +813,13 @@ class ContactUsView(LoginRequiredMixin, TemplateView):
 
 
 
-# 
+#
 # def set_session(request):
 #     request.session['username'] = 'reanjie'
 #     request.session['age'] = '18'
 #     return HttpResponse('set sessions')
-#     
+#
 # def get_session(request):
-#     username = request.session['username'] 
-#     age = request.session['age'] 
+#     username = request.session['username']
+#     age = request.session['age']
 #     return HttpResponse(username+':'+age)
-    
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
--- dbbus/static/js/serviceworker.js
+++ dbbus/static/js/serviceworker.js
@@ -1,26 +1,11 @@
-var staticCacheName = 'djangopwa-v1';
+function preCache() {
+  return cache.open('my-cache').then(function(cache) {
+    return cache.addAll([
+      '{% url "user/index" %}'
+    ]);
+  });
+}
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/IndexView'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  var requestUrl = new URL(event.request.url);
-    if (requestUrl.origin === location.origin) {
-      if ((requestUrl.pathname === '/')) {
-        event.respondWith(caches.match('/IndexView'));
-        return;
-      }
-    }
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
+  event.waitUntil(preCache());
 });
