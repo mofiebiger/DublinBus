3182a224649d92f3771623c9e4a417187482169c
Administrator <renjie.fu@ucdconnect.ie>
2019-07-23 11:15:12 +0100

    add contact ajax function

--- dbbus/static/js/login.js
+++ dbbus/static/js/login.js
@@ -7,13 +7,14 @@ $(function(){
 	$('#loginBtn').on('click',function(){
 		var username = $('#username').val()
 		var pwd = $('#pwd').val()
-
+		var token = $('input[name="csrfmiddlewaretoken"]').val()
+		alert(token);
 		$.ajax({
 			cache:false,
 			type: "POST",
 			// url:"/user/Login_form_post",
 			url:"http://127.0.0.1:8000/user/login",
-			data:{'username':username, 'pwd': pwd},
+			data:{'username':username, 'pwd': pwd,'csrfmiddlewaretoken':token},
 			// data:{'user_name':name},
 			// dataType:'json',
 			async:true,
@@ -36,5 +37,28 @@ $(function(){
 		});
 	});
 
+	$('#contact_info').submit(function(){
+		var contact = $('textarea[name="contact"]').val()
+		var token = $('input[name="csrfmiddlewaretoken"]').val()
+		$.ajax({
+			cache:false,
+			type: "POST",
+			url: window.location.protocol+"//"+window.location.host+"/user/contact",
+			data:{'contact': contact,'csrfmiddlewaretoken':token},
+			dataType:'json',
+			
+			async:false,
+
+			}).done(function(result){
+				 if(result.res == 1){
+					 window.confirm(result.success_msg);
+                    }else {
+                    	window.confirm(result.error_msg);
+				 	}
+			}).fail(function(){
+				alert("false1");
+			});
+		return false;
+	});
 
 })
