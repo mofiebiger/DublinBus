053672d77e789ba1558ed0f558565ef4a2f72b44
Administrator <renjie.fu@ucdconnect.ie>
2019-08-13 12:51:53 +0100

    delete the copy function

--- dbbus/apps/user/urls.py
+++ dbbus/apps/user/urls.py
@@ -19,8 +19,6 @@ urlpatterns = [
 
     # '''meihan added'''
     path('stops', views.StopsView.as_view(), name = 'stops'),
-
-    path('favourites', views.FavouritesView.as_view(), name = 'favourites'),
     path('bus_info', views.BusInfoView.as_view(), name = 'bus_info'),
     path('stop_info', views.StopInfoView.as_view(), name = 'stop_info'),
     path('TrafficFeed', views.TrafficFeedView.as_view(), name = 'TrafficFeed'),
